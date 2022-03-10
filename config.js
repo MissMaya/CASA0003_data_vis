861a6dd64d24084dcf38a0b3b8e200affb3d7cde
MissMaya <nayomi.kasthuriarachchi.21@ucl.ac.uk>
2022-03-10 11:10:04 +0000

    updating after removing legend styling

--- config.js
+++ config.js
@@ -29,13 +29,8 @@ let divChapter1 =
     '<h3>Before the Bang</h3>' +
     '<img src="images/Chapter_1_Image.jpg"></img>' +
     '<p class="imageCredit"><a href="https://www.alamy.com/stock-photo-district1888-p065-bank-of-england-map-73513018.html?pv=1&stamp=2&imageid=D4B87983-BDCF-4DED-A2FA-64ECB8A86F16&p=221233&n=0&orientation=0&pn=1&searchtype=0&IsFromSearch=1&srch=foo%3dbar%26st%3d0%26pn%3d1%26ps%3d100%26sortby%3d2%26resultview%3dsortbyPopular%26npgs%3d0%26qt%3dengland%2520map%26qt_raw%3dengland%2520map%26lic%3d3%26mr%3d0%26pr%3d0%26ot%3d0%26creative%3d%26ag%3d0%26hc%3d0%26pc%3d%26blackwhite%3d1%26cutout%3d%26tbar%3d1%26et%3d0x000000000000000000000%26vp%3d0%26loc%3d0%26imgt%3d0%26dtfr%3d%26dtto%3d%26size%3d0xFF%26archive%3d1%26groupid%3d%26pseudoid%3d%26a%3d%26cdid%3d%26cdsrt%3d%26name%3d%26qn%3d%26apalib%3d%26apalic%3d%26lightbox%3d%26gname%3d%26gtype%3d%26xstx%3d0%26simid%3d%26saveQry%3d%26editorial%3d1%26nu%3d%26t%3d%26edoptin%3d%26customgeoip%3d%26cap%3d1%26cbstore%3d1%26vd%3d0%26lb%3d%26fi%3d2%26edrf%3d%26ispremium%3d1%26flip%3d0%26pl%3d">Source: Alamy</a></p>' +
-    '<p>Before the bang, outline the restrictions 10 mins of BoE. Outline the lack of suitable space</p>'+
-    '<hr>' +
-    '<div id="legend" class="legend"></div>' +
-    '<p>Year of Construction<p>' +
-    '<div><span style="background-color: #ff4400; height:20px; width:20px"></span></div>' +
-    '</div>';
-
+    '<p>Before the bang, outline the restrictions 10 mins of BoE. Outline the lack of suitable space</p>';
+    
 let divChapter2 =
     '<h3>Broadgate</h3>' +
     '<img src="images/Chapter_2_Image.jpg"></img>' +
